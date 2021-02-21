4335f41c685ab0b6a0be02d7397e7a64b319a2e1
Aziz Booker <bookeraziz@gmail.com>
2021-02-21 14:42:10 -0500

    finshe fs demo

---
+++ crash course/refrence/fs_demo.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+//Create Folder
+
+fs.mkdir(path.join(__dirname, "./test"), {}, (err) => {
+  if (err) throw err;
+  console.log("Folder Created...");
+});
+
+//Create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World!",
+  (err) => {
+    if (err) throw err;
+    console.log("File Wiritten to...");
+    //FIle Append
+    fs.appendFile(
+        path.join(__dirname, "/test", "hello.txt"),
+        "\nI love Node.js its so cool",
+        (err) => {
+          if (err) throw err;
+          console.log("File Wiritten to...");
+        }
+      );
+      
+    
+  }
+);
+
+
+fs.readFile(path.join(__dirname, "/test",'hello.txt'), 'utf8', (err,data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+
+//Rename File
+fs.rename(path.join(__dirname, "/test",'hello.txt'),path.join(__dirname, "/test",'helloworld.txt'), (err) => {
+    if (err) throw err;
+    console.log('file renamed...');
+  });
