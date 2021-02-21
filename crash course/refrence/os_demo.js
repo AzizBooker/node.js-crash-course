8495bdc8a0781e802f6dd4eeeb2f457bb21087ed
Aziz Booker <bookeraziz@gmail.com>
2021-02-21 14:47:21 -0500

    finshed os_demo

--- crash course/refrence/os_demo.js
+++ crash course/refrence/os_demo.js
@@ -1,4 +1,27 @@
 const os=require('os')
 
-//
+//Platform
 
+console.log(os.platform());
+
+//CPU Arch
+
+console.log(os.arch())
+
+console.log(os.cpus())
+
+//Free Memory
+
+console.log(os.freemem())
+
+//Total Memory
+console.log(os.totalmem())
+
+
+//Home Dir
+
+console.log(os.homedir())
+
+//Uptime how long system has been up
+
+console.log(os.uptime())
